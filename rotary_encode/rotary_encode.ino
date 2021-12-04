45e8aab7babf77b574792c7cc9117854746cb5e4
touka_aoi <58030947+touka-aoi@users.noreply.github.com>
2021-12-05 05:12:29 +0900

    Revert "失敗ラグが出てしまってどうしようもない、原則I/Oは最小でやった方がいい"
    This reverts commit a8c7e8531d624fc8000a6970fd9f6d89aac2ec3d.

--- rotary_encode/rotary_encode.ino
+++ rotary_encode/rotary_encode.ino
@@ -3,7 +3,6 @@
 char encoder_a = 2; //エンコーダのA相
 char encoder_b = 3; //エンコーダのB相
 int encoder_cnt = 0; //カウントアップ用変数
-int before_cnt = 0; //以前のカウンタ
 
 void setup() {
   pinMode(encoder_a, INPUT_PULLUP); //A相用入力、プルアップに設定
@@ -13,8 +12,7 @@ void setup() {
 }
 
 void loop() {
-  Serial.println(enocer_judge(encoder_cnt)); //カウントをシリアルで送る
-  encoder_cnt %= 600; //600pのため600で切り捨て
+  Serial.println(encoder_cnt); //カウントをシリアルで送る
   delay(10);
 }
 
@@ -27,15 +25,4 @@ void encoder_pulse() {
   else if (digitalRead(encoder_b) == 1) {
     encoder_cnt++;
   }
-}
-
-//増加か減少かを測る関数
-int enocer_judge(int now_cnt) {
-  if (before_cnt < now_cnt) {
-    return 1;
-  }
-  else {
-    return -1;
-  }
-  before_cnt = now_cnt;
 }
